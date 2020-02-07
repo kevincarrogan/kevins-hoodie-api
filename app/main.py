c3fb42e73276d49b93d20b6112d4e1c9429b19e2
Kevin Carrogan <kevin.carrogan@gmail.com>
2020-02-07 12:28:52 +0000

    Add post action for days endpoint

--- app/main.py
+++ app/main.py
@@ -1,12 +1,27 @@
 import httpx
 import json
 
+from datetime import date
+from typing import List
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+from pydantic.color import Color
 
 app = FastAPI()
 
 
-@app.get("/days/")
+class Day(BaseModel):
+    colour: str = None
+    hex: Color = None
+    date: date
+
+
+class Days(BaseModel):
+    days: List[Day]
+
+
+@app.get("/days/", response_model=Days)
 async def days():
     colours_url = "https://raw.githubusercontent.com/kevincarrogan/whatcolourhoodieiskevinwearingtoday.com/master/data/colours.json"
 
@@ -14,3 +29,8 @@ async def days():
         response = await client.get(colours_url)
 
     return {"days": response.json()}
+
+
+@app.post("/days/", response_model=Day)
+def add_day(day: Day):
+    return day
