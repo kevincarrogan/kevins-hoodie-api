195c32d85f9e9d3a37b4f899f9303e4d3c3f35bc
Kevin Carrogan <kevin.carrogan@gmail.com>
2020-02-08 21:20:19 +0000

    Add API key auth header

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 import boto3
 import httpx
 import json
+import os
 
 from datetime import date
 from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Security
+from fastapi.security import APIKeyHeader
 from pydantic import BaseModel
 from pydantic.color import Color
+from starlette.status import HTTP_403_FORBIDDEN
 
 app = FastAPI()
 
@@ -27,8 +30,14 @@ class Days(BaseModel):
     days: List[Day]
 
 
+api_key_header = APIKeyHeader(name="api-key")
+api_key = os.environ["API_KEY"]
+
+
 @app.get("/days/", response_model=Days)
-async def days():
+async def days(*, api_header: str = Security(api_key_header)):
+    if api_header != api_key:
+        raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="Not authenticated")
     colours_url = "https://raw.githubusercontent.com/kevincarrogan/whatcolourhoodieiskevinwearingtoday.com/master/data/colours.json"
 
     async with httpx.AsyncClient() as client:
